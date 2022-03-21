9b14cd4f4f70b42358ac227f49a1e681514bd93f
tahsinur <tahsinur777@gmail.com>
2022-03-21 22:06:58 +0600

    department delete, showAll,showbyid api done

--- APIAssignment/APIAssignment/Controllers/DepartmentController.cs
+++ APIAssignment/APIAssignment/Controllers/DepartmentController.cs
@@ -12,7 +12,7 @@ namespace APIAssignment.Controllers
 {
     public class DepartmentController : ApiController
     {
-        [Route("api/Create/Dept")]
+        [Route("api/Dept/Create")]
         [HttpPost]
         public HttpResponseMessage Create(DepartmentModel deptModel)
         {
@@ -29,5 +29,48 @@ namespace APIAssignment.Controllers
             }
             return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
         }
+        [Route("api/Dept/ShowAll")]
+        [HttpGet]
+        public HttpResponseMessage ShowAll()
+        {
+            APIAssignmentEntities db = new APIAssignmentEntities();
+            var dept = db.Departments.ToList();
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Department, DepartmentModel>());
+            var mapper = new Mapper(config);
+            var deptModel = mapper.Map<List<DepartmentModel>>(dept);
+            return Request.CreateResponse(HttpStatusCode.OK, deptModel);
+        }
+
+        [Route("api/Dept/Show/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Show(int id)
+        {
+            APIAssignmentEntities db = new APIAssignmentEntities();
+            var dept = (from x in db.Departments
+                        where x.Id.Equals(id)
+                        select x).FirstOrDefault();
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Department, DepartmentModel>());
+            var mapper = new Mapper(config);
+            var deptModel = mapper.Map<DepartmentModel>(dept);
+            return Request.CreateResponse(HttpStatusCode.OK, deptModel);
+        }
+
+        [Route("api/Dept/Delete/{id}")]
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            APIAssignmentEntities db = new APIAssignmentEntities();
+            var dept = (from x in db.Departments
+                        where x.Id.Equals(id)
+                        select x).FirstOrDefault();
+
+            db.Departments.Remove(dept);
+            db.SaveChanges();
+            
+            return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+        }
+
     }
 }
